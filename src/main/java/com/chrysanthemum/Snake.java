e0deb0b1f8316f4051689f01a168865f5188a8ab
omnifocus <omnifocus@protonmail.com>
2020-08-03 21:44:07 +0800

    重构snake，一个snake由多个node组成

--- src/main/java/com/chrysanthemum/Snake.java
+++ src/main/java/com/chrysanthemum/Snake.java
@@ -10,27 +10,36 @@ import java.awt.*;
  */
 ///不用继承Frame
 public class Snake /*extends Frame*/ {
-    public static final int ROW;
-    public static final int COL;
-    public static final int SIZE = Yard.BLOCK_SIZE;
-    private static int base_rand = 201;
-    private static int base_rand_complement = 100;
 
-    static {
-        // 刚好占满一个格子
-        int rand_row = (int) (Math.random() * base_rand) + base_rand_complement;
-        int rand_col = (int) (Math.random() * base_rand) + base_rand_complement;
-        // 如果是202，那么就202-2，如果是200，结果就是200
-        ROW = rand_row % 10 == 0 ? rand_row : rand_row - rand_row % 10;
-        COL = rand_col % 10 == 0 ? rand_col : rand_col - rand_col % 10;
+
+    // 由多个Node组成
+    private Node head ;
+    private Node tail;
+
+
+
+    /*至于怎么画是每一节做决定*/
+    private class Node {
+        public static final int SIZE = Yard.BLOCK_SIZE;
+        public final int ROW;
+        public  final int COL;
+        private  int base_rand = 201;
+        private  int base_rand_complement = 100;
+       {
+            // 刚好占满一个格子
+            int rand_row = (int) (Math.random() * base_rand) + base_rand_complement;
+            int rand_col = (int) (Math.random() * base_rand) + base_rand_complement;
+            // 如果是202，那么就202-2，如果是200，结果就是200
+            ROW = rand_row % 10 == 0 ? rand_row : rand_row - rand_row % 10;
+            COL = rand_col % 10 == 0 ? rand_col : rand_col - rand_col % 10;
+        }
+
+        public void paint(Graphics g) {
+            Color c = g.getColor();
+            g.setColor(Color.BLACK);
+            g.fillRect(ROW, COL, SIZE, SIZE);
+            g.setColor(c);
+        }
+
     }
-
-    public void paint(Graphics g) {
-        Color c = g.getColor();
-        g.setColor(Color.BLACK);
-        g.fillRect(ROW, COL, SIZE, SIZE);
-        g.setColor(c);
-    }
-
-
 }
